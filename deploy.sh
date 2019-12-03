515c02bfb6d29e3db9c33fe90991fee1b6505866
이두호 <ddu0422@naver.com>
2019-12-03 21:47:54 +0900

    e

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,1 @@
-apt-get update
-apt-get install curl
-curl -sL https://deb.nodesource.com/setup_12.x | bash -
-apt install nodejs
-cd $WORKSPACE/src/frontend
-npm install
 sh $WORKSPACE/gradlew build
